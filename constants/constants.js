9d4fffd33d43d6d44dfcf61f647fe2b687f6ad31
kartikkrishnasharmaa <kartikkrishnasharmaa@gmail.com>
2024-08-02 15:57:49 +0530

    commit msg

---
+++ constants/constants.js
@@ -0,0 +1,65 @@
+export const NAME = 'Kartik Sharma';
+
+export const DESIGNATION = `MERN Stack Developer | Data Analyst | Tech Blogger`;
+
+export const DETAILS = {
+  Residence: 'India',
+  City: 'Sikar',
+  Age: '26',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '75%',
+  },
+  {
+    title: 'React Developer',
+    level: '91%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Backend Developer',
+    level: '54%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '83%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'ReactJS',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Power BI',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'SQL',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'kartik.thedeveloper@gmail.com',
+  PHONE: '+918302286091',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/kartikkrishnasharmaa',
+  LINKEDIN: 'https://www.linkedin.com/in/',
+};
