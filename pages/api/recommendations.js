f384e2ad34d3a69a7dd0dbd181679597833decf1
Kartik Sharma ( Krishna ) <kartikkrishnasharmaa@gmail.com>
2024-11-20 17:14:51 +0530

    Updated code  with new content

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -3,7 +3,7 @@ const recommendationCard = [
   {
     id: 0,
     name: 'Gautam Sharma',
-    image: "images/shahmir.jfif",
+    image: "images/gautam.png",
     designation: 'React JS | NEXT JS | Node | GraphQL',
     view: "Hey everyone! I wanted to take a moment to recommend Kartik Sharma for any React.js opportunities. I had the pleasure of working with him on multiple projects where he showcased a strong understanding of React.js and Next.js concepts and delivered high-quality code. Highly recommended! 👍! 🌟",
     linkednURL: ""
@@ -12,7 +12,7 @@ const recommendationCard = [
   {
     id: 2,
     name: 'Ankit Soni',
-    image: "images/nasir.jpg",
+    image: "images/ankit.png",
     designation: 'MERN | JavaScript | Tailwind | Bootstrap',
     view: "I wholeheartedly recommend Kartik as a talented React frontend developer with an incredible flair for UI/UX design. His proficiency in Node.js further enhances his capabilities, allowing him to build robust and scalable applications.",
     linkednURL: ""
